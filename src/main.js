d923cc61c3af4522b2e0c076a762e7d431fcf8b9
Artur Myronchuk <rolax223@gmail.com>
2023-03-20 02:23:44 +0200

    8776768

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import './https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js';
-import './https://code.jquery.com/jquery-3.6.4.min.js';
 import './js/modal';
 import './js/swiper';
 import './js/slick.min';
