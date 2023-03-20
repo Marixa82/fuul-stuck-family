59fab3db3f433d04b6eced27f26ba0afaef4f7d0
Artur Myronchuk <rolax223@gmail.com>
2023-03-20 21:56:35 +0200

    new comment

--- src/js/modal.js
+++ src/js/modal.js
@@ -23,3 +23,29 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open2  ]'),
+    closeModalBtn: document.querySelector('[data-modal-close2  ]'),
+    modal: document.querySelector('[data-modal2 ]'),
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open3  ]'),
+    closeModalBtn: document.querySelector('[data-modal-close3  ]'),
+    modal: document.querySelector('[data-modal3 ]'),
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
