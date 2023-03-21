0f6b6704bdb24aea70bd438456f99aceaef8a209
Artur Myronchuk <rolax223@gmail.com>
2023-03-21 02:34:09 +0200

    456456456

--- src/js/modal.js
+++ src/js/modal.js
@@ -49,3 +49,16 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open4  ]'),
+    closeModalBtn: document.querySelector('[data-modal-close4  ]'),
+    modal: document.querySelector('[data-modal4 ]'),
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
